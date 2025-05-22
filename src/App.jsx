fd2c661e2e67c906cc4fc05c8c1235e0ac75a64d
Rashedin-063 <rashedinislam.06@gmail.com>
2025-05-22 14:40:45 +0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ const App = () => {
   return (
     <div className=''>
       Twine Chat App
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt accusantium modi velit reiciendis earum aspernatur autem iste cupiditate veniam perspiciatis?</p>
+
+      
     </div>
   )
 }

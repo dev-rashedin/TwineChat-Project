84e2ec09a13b1a6c47dc2962ee8fd28d531e7e84
Rashedin-063 <rashedinislam.06@gmail.com>
2025-05-24 23:26:38 +0600

    ui updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 const App = () => {
   return (
-    <div className=''>
-      Twine Chat App
-
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt accusantium modi velit reiciendis earum aspernatur autem iste cupiditate veniam perspiciatis?</p>
-
-      
+    <div className='container'>
+      <h1> Twine Chat App</h1>
     </div>
-  )
+  );
 }
 
 export default App

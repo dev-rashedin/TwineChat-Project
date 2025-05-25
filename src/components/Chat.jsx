714932f5431fa5ff13f8d5b5b2d1b548ba37a87a
Rashedin-063 <rashedinislam.06@gmail.com>
2025-05-25 13:41:53 +0600

    chat upper part ui done

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,6 +2,30 @@ import '../styles/chat.css';
 
 
 const Chat = () => {
-  return <div className='chat'>Chat</div>;
+  return (
+    <div className='chat'>
+      {/* top portion */}
+      <div className='top'>
+        {/* user */}
+        <div className='user'>
+          <img src='./avatar.png' alt='user' />
+          <div className='texts'>
+            <span>Jane Doe</span>
+            <p>Lorem ipsum dolor sit amet!</p>
+          </div>
+        </div>
+        {/* icons */}
+        <div className='icons'>
+          <img src='./phone.png' alt='phone' />
+          <img src='./video.png' alt='video' />
+          <img src='./info.png' alt='more' />
+        </div>
+      </div>
+      {/* center portion */}
+      <div className='center'></div>
+      {/* bottom portion */}
+      <div className='bottom'></div>
+    </div>
+  );
 };
 export default Chat;

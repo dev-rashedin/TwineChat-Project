80815be39062c27697c5e9d115929c549bb259c3
Rashedin-063 <rashedinislam.06@gmail.com>
2025-05-25 13:13:49 +0600

    chat ui updated

--- src/components/ChatList.jsx
+++ src/components/ChatList.jsx
@@ -1,14 +1,46 @@
+import { useState } from 'react';
 import '../styles/chatList.css'
 
 const ChatList = () => {
+
+  const [addMode, setAddMode] = useState(false);
+
   return (
     <div className='chatList'>
+      {/* search */}
       <div className='search'>
         <div className='searchBar'>
           <img src='./search.png' alt='search' />
           <input type='text' placeholder='Search...' />
         </div>
-        <img src='./plus.png' alt='add' className='add' />
+        <img
+          src={addMode ? './minus.png' : './plus.png'}
+          alt='add'
+          className='add'
+          onClick={() => setAddMode((prev) => !prev)}
+        />
+      </div>
+      {/*  */}
+      <div className="item">
+        <img src="./avatar.png" alt="user" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello Jane!</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="user" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello Jane!</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="user" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello Jane!</p>
+        </div>
       </div>
     </div>
   );

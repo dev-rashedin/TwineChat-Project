e510d1418d8e45d57ce5081c37a08ab175b841b7
Rashedin-063 <rashedinislam.06@gmail.com>
2025-05-25 20:13:52 +0600

    chat detail ui done

--- src/components/D2.jsx
+++ src/components/D2.jsx
@@ -1,37 +1,66 @@
-import '../styles/detail.css'
+import { arrayRemove, arrayUnion, doc, updateDoc } from 'firebase/firestore';
+import { useChatStore } from '../../lib/chatStore';
+import { auth, db } from '../../lib/firebase';
+import { useUserStore } from '../../lib/userStore';
+import './detail.css';
 
 const Detail = () => {
+  const {
+    chatId,
+    user,
+    isCurrentUserBlocked,
+    isReceiverBlocked,
+    changeBlock,
+    resetChat,
+  } = useChatStore();
+  const { currentUser } = useUserStore();
+
+  const handleBlock = async () => {
+    if (!user) return;
+
+    const userDocRef = doc(db, 'users', currentUser.id);
+
+    try {
+      await updateDoc(userDocRef, {
+        blocked: isReceiverBlocked ? arrayRemove(user.id) : arrayUnion(user.id),
+      });
+      changeBlock();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleLogout = () => {
+    auth.signOut();
+    resetChat();
+  };
+
   return (
     <div className='detail'>
       <div className='user'>
-        <img src='./avatar.png' alt='User' />
-        <h2>Jane Doe</h2>
+        <img src={user?.avatar || './avatar.png'} alt='' />
+        <h2>{user?.username}</h2>
         <p>Lorem ipsum dolor sit amet.</p>
       </div>
-
       <div className='info'>
-        {/* 1st option */}
         <div className='option'>
           <div className='title'>
             <span>Chat Settings</span>
-            <img src='./arrowUp.png' alt='Setting' />
+            <img src='./arrowUp.png' alt='' />
           </div>
         </div>
-        {/* 2nd option */}
         <div className='option'>
           <div className='title'>
-            <span>Privacy % help</span>
-            <img src='./arrowUp.png' alt='Setting' />
+            <span>Chat Settings</span>
+            <img src='./arrowUp.png' alt='' />
           </div>
         </div>
-        {/* 3rd option */}
         <div className='option'>
           <div className='title'>
-            <span>Shared Photos</span>
-            <img src='./arrowDown.png' alt='Setting' />
+            <span>Privacy & help</span>
+            <img src='./arrowUp.png' alt='' />
           </div>
         </div>
-        {/* 4th option */}
         <div className='option'>
           <div className='title'>
             <span>Shared photos</span>
@@ -80,18 +109,25 @@ const Detail = () => {
             </div>
           </div>
         </div>
-        {/* 5th option */}
         <div className='option'>
           <div className='title'>
             <span>Shared Files</span>
             <img src='./arrowUp.png' alt='' />
           </div>
         </div>
-
-        {/* block button */}
-        <button>Block User</button>
+        <button onClick={handleBlock}>
+          {isCurrentUserBlocked
+            ? 'You are Blocked!'
+            : isReceiverBlocked
+            ? 'User blocked'
+            : 'Block User'}
+        </button>
+        <button className='logout' onClick={handleLogout}>
+          Logout
+        </button>
       </div>
     </div>
   );
-}
-export default Detail
+};
+
+export default Detail;

168e7c34015bb09f80da5a2c2b414aee44e8c926
Rashedin-063 <rashedinislam.06@gmail.com>
2025-05-28 09:52:25 +0600

    updated

--- Client/src/lib/firebase.js
+++ Client/src/lib/firebase.js
@@ -2,6 +2,9 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore';
 
+console.log(import.meta.env.VITE_APP_ID);
+
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,

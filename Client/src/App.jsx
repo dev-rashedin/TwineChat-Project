7fe55166bf7ae492a5355c0db0c1247c85f8a505
Rashedin-063 <rashedinislam.06@gmail.com>
2025-07-22 20:14:08 +0600

    user info stored in the userStore, and fethced using useEffect

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -6,20 +6,30 @@ import Login from "./components/Login";
 import Notification from "./components/Notification";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./lib/firebase";
+import { useUserStore } from "./lib/userStore";
 
 const App = () => {
+
+  const {currentUser, isLoading, fetchUserInfo} = useUserStore()
 
   const user = true;
 
   useEffect(() => {
     const unSub = onAuthStateChanged(auth, (user) => {
-
+    fetchUserInfo(user?.uid)
     })
 
     return () => {
       unSub();
     }
-  },[])
+  }, [fetchUserInfo])
+
+
+  console.log(currentUser)
+  
+  
+
+  if(isLoading) return <div className="loading">Loading...</div>
 
   return (
     <div className='container'>
